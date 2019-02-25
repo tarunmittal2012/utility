f45d3d65f9e7c299cb18a2089a03ed9447efdb4d
Tarun Mittal <30505734+tarunmittal2012@users.noreply.github.com>
2019-02-25 23:44:29 +0530

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using Android.Support.Design.Widget;
+using Android.Support.V7.Widget;
+using Android.Util;
+namespace Utility
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Android.App.Fragment[] _fragments;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            _fragments = new Android.App.Fragment[]
+                             {
+                             new WelcomeFragment(),
+                             new ManageFragment(),
+                             new ShipFragment()
+                             };
+            ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+            SetContentView(Resource.Layout.activity_main);
+
+            AddTabToActionBar(Resource.String.tab1);
+            AddTabToActionBar(Resource.String.tab2);
+            AddTabToActionBar(Resource.String.tab3);
+
+        }
+        void AddTabToActionBar(int labelResourceId)
+        {
+            var tab = ActionBar.NewTab()
+                                         .SetText(labelResourceId);
+            
+            tab.TabSelected += TabOnTabSelected;
+            ActionBar.AddTab(tab);
+        }
+        void TabOnTabSelected(object sender, Android.App.ActionBar.TabEventArgs tabEventArgs)
+        {
+            var tab = (Android.App.ActionBar.Tab)sender;
+
+            //  Log.Debug(Tag, "The tab {0} has been selected.", tab.Text);
+            Android.App.Fragment frag = _fragments[tab.Position];
+            tabEventArgs.FragmentTransaction.Replace(Resource.Id.frameLayout1, frag);
+        }
+    }
+}
---
+++ SplashActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Utility
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    class SplashActivity : AppCompatActivity
+    {
+        public override void OnBackPressed()
+        {
+       //     base.OnBackPressed();
+        }
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            Task startupWork = new Task(() => { SimulateStartUp(); });
+
+            base.OnResume();
+        }
+        async void SimulateStartUp()
+        {
+            await Task.Delay(8000); // Simulate a bit of startup work.
+
+            StartActivity(new Intent(ApplicationContext, typeof(MainActivity)));
+        }
+    }
+}
